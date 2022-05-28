c33112d8b89a89e80c9b8bade6914e05db56aacb
Rahul Mishra <rahulmishra21791@gmail.com>
2022-05-28 17:47:14 +0530

    asnwer

--- template-literal.js
+++ template-literal.js
@@ -9,3 +9,11 @@ const age = 21;
 
 getPersonInfo`${person} is ${age} years old`;
 getPersonInfo`hey there`;
+
+// A: "Lydia" 21 ["", " is ", " years old"]
+// B: ["", " is ", " years old"] "Lydia" 21
+
+// Answer: B
+// If you use tagged template literals,
+// the value of the first argument is always an array of the string values.
+// The remaining arguments get the values of the passed expressions!
